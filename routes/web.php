27263e0d15cffcbde61dccd14349eb557459ffb9
JorgeFullScout777 <jorgefullscout@gmail.com>
2024-03-21 10:54:45 -0600

    cambios

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TeEventController;
+//use App\Http\Controllers\TeEventController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('test', [TeEventController::class, 'testingevent']);
+//Route::get('test', [TeEventController::class, 'testingevent']);
