95b19e0edfc2a6d414044da2f65e7d0f4fb16195
AlVielma <vielma7220>
2024-04-08 20:11:14 -0600

    levantar servidor de websocket

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,14 +36,11 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'host' => '127.0.0.1',
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'useTLS' => true,	
+                'host' => env('PUSHER_APP_PATH'),
                 'port' => 6001,
-                'scheme' => 'http',
-                'encrypted' => true,
-                //'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
-            ],
-            'client_options' => [
-                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
+                'scheme' => 'http'
             ],
         ],
 
